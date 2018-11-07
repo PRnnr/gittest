e1cca747ce19d82235775a08e163853359af580c
patrick renner <prenner@daenet.com>
2018-11-07 12:33:53 +0000

    Your Message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //jfiuifuh
+            //jfiuifua fawdh
+            var x = 1;
+            
+           
         }
     }
 }
